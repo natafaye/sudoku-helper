36044d9f3c42961be335f10f8e7bf757a0ed3e57
Natalie Childs <nata.faye@gmail.com>
2025-11-04 12:40:20 -0800

    Fixed table borders

--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -1,12 +1,6 @@
 import type { PossibilityGroup } from '../types';
 import { DIGITS, EXCLUDED_VARIANT, INCLUDED_VARIANT } from '../utilities/constants'
 
-const getCellClass = (digit: number, excludedDigits: number[], includedDigits: number[]) => {
-    if (excludedDigits.includes(digit)) return "table-" + EXCLUDED_VARIANT;
-    if (includedDigits.includes(digit)) return "table-" + INCLUDED_VARIANT;
-    return "";
-}
-
 type Props = {
     results: PossibilityGroup[]
     excludedDigits: number[]
@@ -14,26 +8,31 @@ type Props = {
 }
 
 export default function ResultsList({ results, excludedDigits, includedDigits }: Props) {
-    const dividerBorders = "border-dark border-1";
+    const getCellClass = (digit: number) => {
+        if (excludedDigits.includes(digit)) return "table-" + EXCLUDED_VARIANT;
+        if (includedDigits.includes(digit)) return "table-" + INCLUDED_VARIANT;
+        return "";
+    }
+
     return (
         <table className="table table-striped table-borderless text-center mt-2">
             <thead>
                 <tr>
                     <th></th>
                     {DIGITS.map(d =>
-                        <th key={d} className={getCellClass(d, excludedDigits, includedDigits)}>
+                        <th key={d} className={getCellClass(d)}>
                             {d}
                         </th>
                     )}
                 </tr>
             </thead>
-            <tbody className={dividerBorders + " border-bottom"}>
+            <tbody className={"border-dark border-bottom"}>
                 {results.map(group =>
                     group.possibilities.map((p, index) =>
-                        <tr key={p.join("")} className={index === 0 ? dividerBorders + " border-top" : ""}>
+                        <tr key={p.join("")} className={index === 0 ? "border-dark border-top" : ""}>
                             <td className="table-light">{group.sum}</td>
                             {DIGITS.map(d =>
-                                <td key={d} className={getCellClass(d, excludedDigits, includedDigits)}>
+                                <td key={d} className={getCellClass(d)}>
                                     {p.includes(d) ? d : ""}
                                 </td>
                             )}
